69b6120b2a31a2a786526a6e1729a66be5422bee
K M Jiaul Islam Jibon <54698236+jiaulislam@users.noreply.github.com>
2019-10-11 23:41:49 +0600

    Update C++SirClass.cpp

--- C++SirClass.cpp
+++ C++SirClass.cpp
@@ -46,3 +46,54 @@ int main(){
     ob4.print();
     return 0;
 }
+
+/*
+* Using Operator Overloading
+*/
+    #include <iostream>
+using namespace std;
+
+class OperatorOverloading {
+    int x_value;
+    float y_value;
+  public:
+    OperatorOverloading();
+    OperatorOverloading(int x, float y);
+    OperatorOverloading(float y);
+    void print();
+/* Operator overloading syntax & use of operator overloading */
+    OperatorOverloading operator -(OperatorOverloading any) {
+        OperatorOverloading temp;
+        temp.x_value = x_value - any.x_value;
+        temp.y_value = y_value - any.y_value;
+        return temp;
+    }
+};
+OperatorOverloading::OperatorOverloading() {
+    x_value = 0;
+    y_value = 0;
+}
+OperatorOverloading::OperatorOverloading(int x, float y) {
+    x_value = x;
+    y_value = y;
+}
+OperatorOverloading::OperatorOverloading(float y) {
+    x_value = 10;
+    y_value = y;
+}
+void OperatorOverloading::print() {
+    cout << "X_VALUE IS: " << x_value << " & Y_VALUE IS: " << y_value << endl;
+}
+
+int main() {
+    OperatorOverloading makeNewObjct;
+    makeNewObjct.print();
+    OperatorOverloading object2(5, 10);
+    OperatorOverloading object3(5, 10);
+    makeNewObjct = object2 - object3;
+    object2.print();
+    OperatorOverloading mk3(100);
+    mk3.print();
+    makeNewObjct.print();
+    return 0;
+}
