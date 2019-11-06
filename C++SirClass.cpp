f1c44a0708a906a8961d29ae0d2313e861cd8fec
K M Jiaul Islam Jibon <54698236+jiaulislam@users.noreply.github.com>
2019-11-06 21:53:08 +0600

    Update C++SirClass.cpp

--- C++SirClass.cpp
+++ C++SirClass.cpp
@@ -100,3 +100,94 @@ int main() {
     makeNewObjct.print();
     return 0;
 }
+
+/*b. Explain Chain function calls e.g object.function1().function2().function*) with coding example(use this pointer)*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Test
+{
+
+public:
+
+	Test Display(); // Function 1
+	Test Show();    // Function 2
+	Test Result();  // Function 3
+
+};
+
+Test Test::Display()
+{
+	cout<<"Function 1"<<endl;
+	return *this;  // Return this pointer
+}
+
+Test Test::Show()
+{
+	cout<<"Function 2"<<endl;
+	return *this;
+}
+
+Test Test::Result()
+{
+	cout<<"Result 3"<<endl;
+	return *this;
+}
+
+int main()
+{
+	Test Obj;
+
+	//Here i have called all the three function in a sequence.
+
+	Obj.Display().Show().Result(); //This is called Function Chaining
+
+	getch();
+}
+
+/*C. Write a class named Point3d. This class should contain :
+*Three private member variables m_x, m_x, m_z;
+* A Public member function named setValues() that allows you to set values for  m_x, m_x & m_z
+* A Public member function named print() that prints the point in the following format -> m_x > m_y > m_z */
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Point3d
+{
+    int m_x, m_y, m_z;
+public:
+    Point3d(); // Default constructor
+    void setValues(int x, int y, int z); //
+    void print();
+    ~Point3d(); // Destructor
+};
+Point3d::Point3d() {
+    m_x = 0;
+    m_y = 0;
+    m_z = 0;
+}
+void Point3d::setValues(int x, int y, int z) {
+    m_x = x;
+    m_y = y;
+    m_z = z;
+}
+void Point3d::print() {
+    cout << "m_x: " << m_x << "->" << m_y << "->" << m_z << endl;
+}
+Point3d::~Point3d() {
+    cout <<"Destructor Loading finished" << endl;
+}
+int main()
+{
+	Point3d Obj;
+	Point3d scndObject;
+    Obj.print();
+	scndObject.setValues(1,2,3);
+	scndObject.print();
+	getch();
+}
+
