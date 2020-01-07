04d40621c9e47c4d209a2cea3831cb5d0bedce99
K M Jiaul Islam Jibon <kmjiaulislam@gmail.com>
2020-01-07 21:16:55 +0600

    Create FinalExamPrep.cpp

---
+++ FinalExamPrep.cpp
@@ -0,0 +1,73 @@
+// Write a Class C That have Member Variable Value(Int), Member Function Constructor, Overlode(+,-) & Overload (==, >)
+
+#include <iostream>
+
+class C
+{
+public:
+	C();
+	C(int x);
+	~C();
+	C operator +(C SOME_OB) {
+		C temp;
+		temp.value = value + SOME_OB.value;
+		return temp;
+	}
+	C operator -(C SOME_OB) {
+		C temp;
+		temp.value = value - SOME_OB.value;
+		return temp;
+	}
+	bool operator == (C SOME_OB) {
+		C temp;
+		if (temp.value == SOME_OB.value) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator > (C SOME_OB) {
+		C temp;
+		if (temp.value < SOME_OB.value) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	void print();
+private:
+	int value;
+};
+C::C()
+{
+	value = 0;
+}
+C::C(int x)
+{
+	value = x;
+}
+C::~C()
+{
+}
+void C::print()
+{
+	std::cout << "Class Object Value: " << value << std::endl;
+}
+int main() {
+	C c1(100), c2(20), c3(50), c5(10);
+	C c4;
+	c4 = c1 + (c2 - c3);
+	c4.print();
+	//std::cout << c1 + c2 - c3;
+	if (c1 > c5) {
+		std::cout << "C1 > C5" << std::endl;
+	}
+	if (c1 == c5) {
+		std::cout << "C1 == C5" << std::endl;
+	}
+
+
+	return 0;
+}
