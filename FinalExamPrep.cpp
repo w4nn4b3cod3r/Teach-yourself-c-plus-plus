63916f45a4529d16bd0c6badc2936fc4dd35b1fa
K M Jiaul Islam Jibon <kmjiaulislam@gmail.com>
2020-01-09 13:27:34 +0600

    Update FinalExamPrep.cpp

--- FinalExamPrep.cpp
+++ FinalExamPrep.cpp
@@ -66,3 +66,49 @@ int main() {
 	}
 	return 0;
 }
+
+//Here is a short example that illustrates the basic operation of a vector.
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v; // create zero-length vector
+	int i; 
+	std::cout << "Default Size of vector: " << v.size() << std::endl;
+
+	//put values onto end of vector --
+	//vector will grow as needed 
+	for (i = 0; i < 10; i++) v.push_back(i);
+
+	// display current size of v
+	std::cout << "Now the size of the vector v: " << v.size() << std::endl;
+
+	// display all the contents of the vector
+
+	for (i = 0; i < v.size(); i++) std::cout << " " << v[i];
+	std::cout << std::endl;
+
+	// Put more values onto end of vector again , vector will grow as needed
+	for (i = 0; i < 10; i++) v.push_back(i + 10);
+
+	// now display the size of the vector
+	std::cout << "Again change of size: " << v.size() << std::endl;
+
+	// Now display again the elements of the vector
+
+	for (i = 0; i < v.size(); i++) std::cout << " " << v[i];
+
+	std::cout << std::endl;
+
+	// change content of vector
+	for (i = 0; i < v.size(); i++) v[i] = v[i]*2;
+
+	// contents doubled
+	for (i = 0; i < v.size(); i++) std::cout << v[i] << " ";
+	std::cout << std::endl;
+
+	return 0;
+
+}
